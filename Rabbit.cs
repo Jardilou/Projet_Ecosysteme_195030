4e4d1d589fd74f5f7e32ed5b06ecdef1cdf69f1b
Jardilou <78799495+Jardilou@users.noreply.github.com>
2022-01-03 10:59:30 +0100

    Update Rabbit.cs

--- Rabbit.cs
+++ Rabbit.cs
@@ -9,13 +9,13 @@ namespace Bac3_Projet_Écosystème_195030
     public class Rabbit : PlantEater
     {
         //params : string Name, int maxHP, int maxEnergy, int SightRadius, int ContactRadius, int damage, int MaternityRest = 0
-        public Rabbit(int pregnancycd = 0) :
-            base("Rabbit", 60, 55, 100, 3, 8, pregnancycd)
+        public Rabbit(int MaternityRest = 0) :
+            base("Rabbit", 60, 55, 100, 3, 8, MaternityRest)
         {
         }
-        public override void Reproduce(Worldmap board)
+        public override void Reproduce(Worldmap map)
         {
-            board.Add(new Rabbit(1000), Pos_x, Pos_y);
+            map.Add(new Rabbit(850), Pos_x, Pos_y);
         }
     }
 }
